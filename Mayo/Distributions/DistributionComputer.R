62363691fb56a2e3cab0be0f0249712b6677af81
Justin Hood <swimhoodyswim@yahoo.com>
2019-10-17 17:22:59 -0500

    Started dist identification

---
+++ Mayo/Distributions/DistributionComputer.R
@@ -0,0 +1,37 @@
+setwd("~/Desktop/PSM/Fall 2019/Capstone/Mayo/Distributions")
+rm(list = ls())
+library(fitdistrplus)
+library(logspline)
+### Read Me in
+X <- read.csv("Mayo_order_processing_data.csv",header = TRUE)
+
+### Order Entry
+orderEntry <- X$Order.Entry[!is.na(X$Order.Entry)]
+hist(orderEntry) ## Looks Normal?
+orderEntry.norm <- fitdist(orderEntry, "norm")
+plot(orderEntry.norm)
+orderEntry.norm$aic
+orderEntry.weib <- fitdist(orderEntry, "weibull")
+plot(orderEntry.weib)
+orderEntry.weib$aic
+orderEntry.exp <- fitdist(orderEntry, "exp")
+plot(orderEntry.exp)
+orderEntry.exp$aic
+
+
+
+
+
+
+
+
+
+
+orderEntryVerification <- X$Order.Entry.Verification[!is.na(X$Order.Entry.Verification)]
+oralDrugPrep <- X$Oral.drug.prep[!is.na(X$Oral.drug.prep)]
+ivDrugPrep <- X$IV.drugprep[!is.na(X$IV.drugprep)]
+orderPrepVerification <- X$Order.prep.verification[!is.na(X$Order.prep.verification)]
+dispensing <- X$Dispensing[!is.na(X$Dispensing)]
+oralIncoming <- X$Oral.Incoming[!is.na(X$Oral.Incoming)]
+ivIncoming <- X$IV.Incoming[!is.na(X$IV.Incoming)]
+hist(ivIncoming)
